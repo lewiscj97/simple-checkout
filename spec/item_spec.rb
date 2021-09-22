82c410e4957a3cab8e9294228cf7b5902be76ac5
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-22 10:09:22 +0100

    test(item): second test of get_price method

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,5 +3,10 @@ require 'item'
 describe Item do
   describe "#get_price" do
     it {is_expected.to respond_to(:get_price)}
+
+    it "returns the price of the item in £xx.xx format" do
+      item = Item.new(0.35)
+      expect(item.get_price).to eq "£0.35"
+    end
   end
 end
