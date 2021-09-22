ee5c6a06e554d1c64dfaeb755f15e14de2bcc011
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-22 10:16:04 +0100

    test(item): add new failing test for price format

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,9 +2,14 @@ require 'item'
 
 describe Item do
   describe "#get_price" do
-    it "returns the price of the item in £xx.xx format" do
+    it "returns the price of the item" do
       item = Item.new(0.35)
       expect(item.get_price).to eq "£0.35"
+    end
+
+    it "returns the price in £xx.xx format" do
+      item = Item.new(1.00)
+      expect(item.get_price).to rq "£1.00"
     end
   end
 end
