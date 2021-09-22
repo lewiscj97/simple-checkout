45e2d51db285f29db77ded28d57037a747f832aa
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-22 10:29:39 +0100

    test(checkout): test scan_item functionality

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,7 +1,15 @@
 require 'checkout'
+require 'item'
 
 describe Checkout do
   describe "#scan_item" do
     it {is_expected.to respond_to(:scan_item)}
+    
+    it "stores the item" do
+      item = Item.new(3.00)
+      subject.scan_item(item)
+      expect(subject.items).to include item
+    end
+
   end
 end
